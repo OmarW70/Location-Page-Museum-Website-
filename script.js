9e749c377605e8137b461b1436a95c5efc289c87
OmarW70 <ow8625954@gmail.com>
2026-03-15 01:51:36 +0200

    Refactor toggleDarkMode and add menu button functionality

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function toggleDarkMode() {
 document.body.classList.toggle('dark-mode') ; 
 }
+const menuBtn = document.getElementById("menu-btn");
+const nav = document.querySelector(".navbar ul");
+
+menuBtn.onclick = () => {
+nav.classList.toggle("active");
+};
